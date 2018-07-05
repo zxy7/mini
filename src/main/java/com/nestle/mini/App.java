b5ea947e7534113af488a59d456a0d1b06fd3d26
昝晓玉 <>
2018-07-05 20:49:59 +0800

    first commit

---
+++ src/main/java/com/nestle/mini/App.java
@@ -0,0 +1,13 @@
+package com.nestle.mini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
